892794b7bc2cbddaaf4f13e28f70663c607d1685
Christian Geisler <christian.geisler@hogrefe.com>
2023-02-16 09:52:44 +0100

    add terraform declarations

---
+++ environment/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+module "vpc" {
+  source             = "./vpc"
+  name               = var.name
+  cidr               = var.cidr
+  private_subnets    = var.private_subnets
+  public_subnets     = var.public_subnets
+  availability_zones = var.availability_zones
+  environment        = var.environment
+}
+
+module "security_groups" {
+  source         = "./security-groups"
+  name           = var.name
+  vpc_id         = module.vpc.id
+  environment    = var.environment
+  container_port = var.container_port
+}
+
+module "alb" {
+  source              = "./alb"
+  name                = var.name
+  vpc_id              = module.vpc.id
+  subnets             = module.vpc.public_subnets
+  environment         = var.environment
+  alb_security_groups = [module.security_groups.alb]
+  alb_tls_cert_arn    = var.tsl_certificate_arn
+  health_check_path   = var.health_check_path
+}
+
+module "ecr" {
+  source      = "./ecr"
+  name        = var.name
+  environment = var.environment
+}
+
+module "secrets" {
+  source              = "./secrets"
+  name                = var.name
+  environment         = var.environment
+  application-secrets = var.application-secrets
+}
---
+++ environment/secrets.tfvars
@@ -0,0 +1,6 @@
+application-secrets = {
+  "SOLARIS_WEBHOOK_SECRET"   = "my-solaris-webhook-secret"
+  "DYNAMO_DB_KEY"            = "my-dynamo-db-key"
+  "DYNAMO_DB_SECRET"         = "my-dynamo-db-secret"
+  "API_KEY_X" = "my-api-key"
+}
